6192fa9c5be600c570e924ed777b919e535e0a17
Catarina Rodrigues Erickson <cat@pop-os.localdomain>
2021-06-23 01:14:29 -0300

    fix iteraçao k

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-print('EP1 - MAP3121 \nAutovalores e Autovetores de Matrizes Tridiagonais Simétricas')
-print('Alunas: Catarina e Isabella')
+# print('EP1 - MAP3121 \nAutovalores e Autovetores de Matrizes Tridiagonais Simétricas')
+# print('Alunas: Catarina e Isabella')
 
 #Calcula u de acordo com a heurística de Wilkinson
 def calc_heuristica(alpha_arr, beta_arr, n):
@@ -10,7 +10,7 @@ def calc_heuristica(alpha_arr, beta_arr, n):
         sgn = 1
     else:
         sgn = -1
-    u = alpha_arr[n] + d - sgn*(d**2 + beta_arr[n-1]**2)**(1/2)
+    u = alpha_arr[n] + d - sgn*((d**2 + beta_arr[n-1]**2)**(1/2))
     return u
 
 #Calcula o seno e o cosseno da matriz Q de transformação de Givens
@@ -39,10 +39,10 @@ def qr(alpha_list, beta_list, deslocamento):
 
     #Inicializa vetor para armazenar os autovalores encontrados
     autovalor_arr = np.array([])
-
+    k=0
     #Itera para m = dim, dim-1, ..., 2
     for m in range(dim, 1, -1):
-        k=0
+        
         #Itera enquanto o beta de referência da iteração m for maior do que 10e-6
         while abs(beta_arr[m-2])>=0.000001:
             #Calcula e subtrai ukI de A(k) a partir da iteração k caso haja deslocamento
@@ -89,8 +89,8 @@ def qr(alpha_list, beta_list, deslocamento):
 
                 #Modifica a coluna i
                 alpha_next[i] = c_arr[i]*alpha_arr[i] - s_arr[i]*beta_arr[i]
-                beta_next[i] = -s_arr[i]*alpha_arr[i+1]
                 #Modifica a coluna i+1
+                beta_next[i] = -s_arr[i]*alpha_arr[i+1]
                 alpha_next[i+1] = c_arr[i]*alpha_arr[i+1]
 
                 #Define as diagonais da matriz R*Q1T*...*QiT como ponto de partida para a próxima iteração
@@ -99,8 +99,7 @@ def qr(alpha_list, beta_list, deslocamento):
 
             #Soma A(k+1) e ukI a partir da iteração k caso haja deslocamento
             if k>0 and deslocamento:
-                alpha_arr = alpha_arr + u_arr
-
+                alpha_arr = alpha_arr + u_arr 
 
             #Calcula V
             for i in range(m-1):
@@ -112,7 +111,7 @@ def qr(alpha_list, beta_list, deslocamento):
 
                 #Define as diagonais da matriz V*Q1T*...*QiT como ponto de partida para a próxima iteração
                 v_arr = v_next.copy()
-            k+=1
+            k+=1   
         
         #Guarda o autovalor encontrado na iteração m no vetor de autovalores
         autovalor_arr = np.append(autovalor_arr, alpha_arr[m-1])
@@ -127,5 +126,26 @@ def qr(alpha_list, beta_list, deslocamento):
     #Inverte o vetor dos autovalores para que o autovalor da posição i corresponda ao autovetor da coluna i da matriz V
     autovalor_arr = np.array(list(reversed(autovalor_arr)))
 
-    #Retorna o vetor com os autovalores e a matriz V, nessa ordem
-    return autovalor_arr, v_arr
+    #Retorna o vetor com os autovalores, a matriz V e o número de iterações, nessa ordem
+    return autovalor_arr, v_arr, k
+
+
+def teste_qr(n, deslocamento):
+    alpha_arr = 2*np.ones((n,))
+    beta_arr = -1*np.ones((n-1,))    
+    return qr(alpha_arr, beta_arr, deslocamento)
+
+def teste_analitico(n):    
+    autovalor_analitico = np.array([])
+    autovetor_analitico = np.zeros((n,n))
+    for i in range(1,n+1):
+        lmb = 2*(1-np.cos(i*np.pi/(n+1)))
+        v = 0
+        norma = 0
+        for j in range (1,n+1):
+            v = np.sin(j*i*np.pi/(n+1))
+            norma += (v)**2
+            autovetor_analitico[i-1][j-1] = v
+        autovetor_analitico[i-1] = autovetor_analitico[i-1]/(norma**(1/2))
+        autovalor_analitico = np.append(autovalor_analitico, lmb)
+    return autovalor_analitico, autovetor_analitico
